7e0c3bd4c6a9ffa2d489737c79d1f08f576415e2
Zhekaman <bosseu@mail.ru>
2021-06-17 23:28:06 +0600

    pip list

---
+++ Launch Microsoft Edge (Chromium).py
@@ -0,0 +1,7 @@
+from msedge.selenium_tools import Edge, EdgeOptions
+
+# Launch Microsoft Edge (Chromium)
+options = EdgeOptions()
+options.use_chromium = True
+driver = Edge(options = options)
+driver.get('http://selenium.dev/')
