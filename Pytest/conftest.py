781c080ca7dec06d5be268eee50936fccb804736
Zhekaman <bosseu@mail.ru>
2021-06-29 22:29:33 +0600

    изменения

--- Pytest/conftest.py
+++ Pytest/conftest.py
@@ -1,22 +1,20 @@
 import pytest
 from selenium import webdriver
-from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.webdriver.chrome.options import Options 
 
-# Launch Microsoft Edge (Chromium)
-options = EdgeOptions()
-options.use_chromium = True
-
+options = Options() 
+options.add_experimental_option('prefs', {'intl.accept_languages': user_language}) 
+browser = webdriver.Chrome(options=options) 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: russian or english")
 
 
 @pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
+def language(request):
+    language = request.config.getoption("language")
+    if language == "chrome":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome()
     elif browser_name == "edge":
